fd12abe30aca63f6d852d5350d07bd90432171f0
srepking <shanerepking@gmail.com>
2020-10-13 23:02:44 -0700

    Lesson 6 Activity Complete

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ import requests
 # (or another user name)
 
 if __name__ == "__main__":
-    username = sys.argv[1]
+    #username = sys.argv[1]
 
     # TODO:
     #
     # 1. Retrieve a list of "events" associated with the given user name
     # 2. Print out the time stamp associated with the first event in that list.
+    username = 'JASchilz'
+    user_events = requests.get('https://api.github.com/users/{}/events/public'.format(username))
+    first_event_time = user_events.json()[0]['created_at']
 
-    print("COMPLETE THE TODOs")
+    print("The first public event listed for username {} was created at {}.".format(username, first_event_time))
     
 
 
